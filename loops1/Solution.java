ecf00716e6eb4f9601e9fe5925877ce95e302974
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-03-28 23:51:03 -0600

    Adding solution for loop challenge.

---
+++ loops1/Solution.java
@@ -0,0 +1,31 @@
+// https://www.hackerrank.com/challenges/java-loops-i/problem?h_r=next-challenge&h_v=zen
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n", N, i, N * i);
+        }
+
+        scanner.close();
+    }
+}
+
+
