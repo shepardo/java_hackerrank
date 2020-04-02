a7ac6a130e943cc3936e3b2fa720bc15440894ac
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-01 23:37:47 -0600

    Fixing missing code.

--- currency_formatter/Solution.java
+++ currency_formatter/Solution.java
@@ -5,16 +5,41 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+import java.text.NumberFormat;
 
 public class Solution {
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double payment = scanner.nextDouble();
         scanner.close();
 
         // Write your code here.
-        
+        Locale locale = Locale.US;
+        NumberFormat format = NumberFormat.getCurrencyInstance(locale);
+        Currency currency = Currency.getInstance(locale);
+        format.setCurrency(currency);
+        String us = format.format(payment);
+
+        locale = Locale.FRANCE;
+        format = NumberFormat.getCurrencyInstance(locale);
+        currency = Currency.getInstance(locale);
+        format.setCurrency(currency);
+        String france = format.format(payment);
+
+        locale = Locale.CHINA;
+        format = NumberFormat.getCurrencyInstance(locale);
+        currency = Currency.getInstance(locale);
+        format.setCurrency(currency);
+        String china = format.format(payment);
+
+        locale = new Locale("EN", "IN");
+        format = NumberFormat.getCurrencyInstance(locale);
+        currency = Currency.getInstance(locale);
+        format.setCurrency(currency);
+        String india = format.format(payment);
+
+
         System.out.println("US: " + us);
         System.out.println("India: " + india);
         System.out.println("China: " + china);
