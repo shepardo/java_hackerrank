ca94dbacc7fe1fd54e6f31fdd7a8de01822ff249
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-01 23:18:50 -0600

    Solution for currency formatter challenge.

---
+++ currency_formatter/Solution.java
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/java-currency-formatter/problem
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
