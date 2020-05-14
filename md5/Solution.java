d0f21bbf758970a1818fdb7ee034bd5b2c62dc70
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-05-13 23:34:16 -0500

    Adding solution for "MD5 Java"

---
+++ md5/Solution.java
@@ -0,0 +1,20 @@
+// https://www.hackerrank.com/challenges/java-md5/problem
+import java.io.*;
+import java.util.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.xml.bind.DatatypeConverter;
+public class Solution {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException  {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        byte[] buf = s.getBytes();
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(buf);
+        byte[] digest = md.digest();
+        String myHash = DatatypeConverter.printHexBinary(digest).toLowerCase();
+        System.out.println(myHash);
+    }
+}
