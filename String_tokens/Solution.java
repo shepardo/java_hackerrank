81a3d9ad9931bee0516ac06080363383867ea961
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-05-14 20:00:57 -0500

    Fixing solution for "Java String Tokens"

--- String_tokens/Solution.java
+++ String_tokens/Solution.java
@@ -6,11 +6,16 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
+        String s = scan.nextLine().trim();
         // Write your code here.
+        if (s.equals("")) {
+            System.out.println("0");
+            return;
+        }
+
         String[] parts = s.split("[ !,?._'@]+");
         System.out.println(parts.length);
-        for(String p: parts) {
+        for (String p: parts) {
             System.out.println(p);
         }
         scan.close();
