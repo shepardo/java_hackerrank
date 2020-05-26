18b06772f33833273b6b2d186ea630bb2fd27885
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-05-25 23:02:58 -0500

    Adding solution for "Java 2d array"

---
+++ 2d_array/Solution.java
@@ -0,0 +1,51 @@
+// https://www.hackerrank.com/challenges/java-2d-array/problem
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static int getHourGlassSum(int x, int y, int[][] arr) {
+        if (x + 2 >= arr[0].length) return -1;
+        if (y + 2 >= arr.length) return -1;
+        return 
+            arr[y][x] + arr[y][x + 1] + arr[y][x + 2] +
+            arr[y + 1][x + 1] +
+            arr[y + 2][x] + arr[y + 2][x + 1] + arr[y + 2][x + 2];
+    }
+
+    private static int calculateMaxHourglassSum(int[][] arr) {
+        int max = -1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                int n = getHourGlassSum(j, i, arr);
+                if (n > max) max = n;
+            }
+        }
+        return max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int max = calculateMaxHourglassSum(arr);
+        System.out.println(max);
+        scanner.close();
+    }
+}
