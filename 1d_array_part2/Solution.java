1c508215207122eab1091a2428b2773b5a5fd629
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-05-27 12:59:17 -0500

    An improved solution from user 'Kaisean'

--- 1d_array_part2/Solution.java
+++ 1d_array_part2/Solution.java
@@ -3,10 +3,40 @@ import java.util.*;
 
 public class Solution {
 
+    private static boolean isSolvable(int m, int[] arr, int i) {
+        if (i < 0 || arr[i] == 1) return false;
+        if ((i == arr.length - 1) || i + m > arr.length - 1) return true;
+
+        arr[i] = 1;
+        return isSolvable(m, arr, i + 1) || isSolvable(m, arr, i - 1) || isSolvable(m, arr, i + m);
+    }
+
+    public static boolean doCanWin(int leap, int pos, int[] game) {
+        return isSolvable(leap, game, pos);
+        /*int i = pos;
+        if (i >= game.length) return true;
+        while (i < game.length) {
+            if (game[i] == 0) {
+                if (doCanWin(leap, i + leap, game)) return true;
+                else i++;
+            } else {
+                return false;
+            }
+        }
+        return false;*/
+    }
+
     public static boolean canWin(int leap, int[] game) {
         // Return true if you can win the game; otherwise, return false.
+        return doCanWin(leap, 0, game);
+        /*
         int cnt = 0;
         int max = 0;
+        int j = 0;
+        while (j < game.length && game[j] == 0) {
+            j++;
+        }
+        j--;
         for (int i = 0; i < game.length; i++) {
             if (game[i] == 1) cnt++;
             else {
@@ -14,7 +44,8 @@ public class Solution {
                 cnt = 0;
             }
         }
-        return max < leap;
+        if (cnt != 0 && cnt > max) return cnt < leap && j + leap >= game.length;
+        return max < leap && j + leap >= game.length;*/
     }
 
     public static void main(String[] args) {
