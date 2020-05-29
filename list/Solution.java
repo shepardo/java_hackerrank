37d16208473966c8e12e4315c9f25a46e267d80c
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-05-28 23:54:38 -0500

    Adding complete version for "Java List"

--- list/Solution.java
+++ list/Solution.java
@@ -7,5 +7,25 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<Integer> a = new ArrayList<>();
+        while (n-- > 0) {
+            a.add(scanner.nextInt());
+        }
+        n = scanner.nextInt();
+        while (n-- > 0) {
+            String op = scanner.next();
+            if (op.equals("Insert")) {
+                int idx = scanner.nextInt();
+                int v = scanner.nextInt();
+                a.add(idx, v);
+            } else if (op.equals("Delete")) {
+                a.remove(scanner.nextInt());
+            }
+        }
+        for (Integer i : a) {
+            System.out.printf("%d ", i);
+        }
     }
 }
