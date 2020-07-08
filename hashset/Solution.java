35ac4958e7ae274848f07a1b4687ff323df03735
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-07-07 23:58:27 -0500

    Update Solution.java

--- hashset/Solution.java
+++ hashset/Solution.java
@@ -17,8 +17,18 @@ public class Solution {
             pair_left[i] = s.next();
             pair_right[i] = s.next();
         }
-
+        
 //Write your code here
-
+        HashSet<String> left_set = new HashSet<>();
+        HashSet<String> right_set = new HashSet<>();
+        for (int i = 0; i < pair_left.length; i++) {
+            left_set.add(pair_left[i]);
+            right_set.add(pair_right[i]);
+            if (left_set.size() > right_set.size()) {
+                System.out.println(left_set.size());
+            } else {
+                System.out.println(right_set.size());
+            }
+        }
    }
 }
