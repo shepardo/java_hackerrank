6e9d504c3cc1cae7378d57da645dfd126763dc42
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-07-07 23:49:11 -0500

    Adding solution "Java Generics"

---
+++ generics/Solution.java
@@ -0,0 +1,36 @@
+// https://www.hackerrank.com/challenges/java-generics/problem
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+class Printer
+{
+   //Write your code here
+    public <T> void printArray(T[] a) {
+        for(T x: a) {
+            System.out.println(x);
+        }
+    }
+}
+
+public class Solution {
+
+
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+      
+    }
+}
