7fe5161d36cf558ca78e538f278b0cffd06dad96
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-07-17 23:59:37 -0500

    Final version for "java exception handling try catch".

--- exception_handling_try_catch/Solution.java
+++ exception_handling_try_catch/Solution.java
@@ -6,5 +6,20 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        String s_x = scanner.next();
+        String s_y = scanner.next();
+        try { 
+            int x = Integer.parseInt(s_x);
+            int y = Integer.parseInt(s_y);
+            int z = x / y;
+            System.out.println(z);
+        } catch(ArithmeticException ae) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        } catch(InputMismatchException ime) {
+            System.out.println("java.util.InputMismatchException");
+        } catch(NumberFormatException nfe) {
+            System.out.println("java.util.InputMismatchException");
+        }
     }
 }
