0522dc60a66bc621f5491910ecf327780288c639
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-07-17 23:51:24 -0500

    Adding solution for "java exception handling try catch".

---
+++ exception_handling_try_catch/Solution.java
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/java-exception-handling-try-catch/problem
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
