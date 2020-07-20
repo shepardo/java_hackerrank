8c440ae88d5fb2064f39397eade37c7f60b4e944
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-07-19 22:26:18 -0500

    Final verision for "Java Inheritance 2"

--- inheritance2/Solution.java
+++ inheritance2/Solution.java
@@ -7,6 +7,15 @@ import java.math.*;
 import java.util.regex.*;
 
 //Write your code here
+class Arithmetic {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic {
+    
+}
 
 public class Solution{
     public static void main(String []args){
