9e0b08ec0bd54969aa2c6e131f68f934e116d90d
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-07-24 10:24:01 -0500

    Checkpoint solution for "Java deque", still some test cases failing.

--- dequue/Solution.java
+++ dequue/Solution.java
@@ -1,16 +1,32 @@
 // https://www.hackerrank.com/challenges/java-dequeue/problem
-import java.util.*;
+    import java.util.*;
     public class test {
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
-            Deque deque = new ArrayDeque<>();
+            Deque<Integer> deque = new ArrayDeque<>();
+            Integer[] a = new Integer[0];
             int n = in.nextInt();
             int m = in.nextInt();
 
             for (int i = 0; i < n; i++) {
                 int num = in.nextInt();
-               
+                deque.add(num);
             }
+            a = deque.toArray(a);
+
+            int max = 0;
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int j = 0; j < n - m; j++) {
+                map.clear();
+                for (int i = 0; i < m; i++) {
+                    map.put(a[i + j], 1);
+                }
+                int max_local = map.size();
+                if (max < max_local) {
+                    max = max_local;
+                }
+            }
+            System.out.println(max);
         }
     }
 
