c2e7770d4ca11720ab977d8a698f96717d846ba4
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-08-04 22:25:54 -0500

    Final solution for "Java BigDecimal".

--- big_decimal/Solution.java
+++ big_decimal/Solution.java
@@ -1,3 +1,4 @@
+// https://www.hackerrank.com/challenges/java-bigdecimal/problem
 import java.math.BigDecimal;
 import java.util.*;
 class Solution{
@@ -12,6 +13,18 @@ class Solution{
         sc.close();
 
         //Write your code here
+        class ComparatorBigDecimals implements Comparator<String> {
+            public int compare(String s1, String s2) {
+                try {
+                    return -(new BigDecimal(s1)).compareTo(new BigDecimal(s2));
+                } catch( Exception e) {
+                    System.out.printf("Error at %s %s", s1, s2);
+                    throw e;
+                }
+            }
+        }
+        s = Arrays.copyOf(s, n);
+        Arrays.sort(s, new ComparatorBigDecimals());
 
         //Output
         for(int i=0;i<n;i++)
