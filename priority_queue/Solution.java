10814b09a6b0c67f107596c25a39c41146c5e905
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-08-23 22:08:57 -0500

    Next checkpoint solution for "Priority queue", still several test cases failing.

--- priority_queue/Solution.java
+++ priority_queue/Solution.java
@@ -1,9 +1,112 @@
+// https://www.hackerrank.com/challenges/java-priority-queue/problem
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 /*
  * Create the Student and Priorities classes here.
  */
+
+class Student implements Comparable<Student> {
+
+  private int id;
+  private String name;
+  private double cgpa;
+  
+  public Student(int id, String name, double cgpa) {
+    this.id = id;
+    this.name = name;
+    this.cgpa = cgpa;
+  }
+
+  public int getId() { return id; }
+  public String getName() { return name; }
+  public double getCgpa() { return cgpa; }
+
+  public String toString() {
+      return String.format("%s %g %d\n", name, cgpa, id);
+  }
+
+  public int compareTo(Student s) {
+    if (this.cgpa < s.cgpa) return -1;
+    else if (this.cgpa > s.cgpa) return 1;
+    else {
+      int result = -this.name.compareTo(s.name);
+      if (result != 0) return result;
+      if (this.id < s.id) return 1; 
+      else if (this.id > s.id) return -1;
+      return 0;
+    }
+  }
+}
+
+class Priorities {
+   private Student[] a;
+   private int n = 0;
+   
+   List<Student> getStudents(List<String> events) {
+     a = new Student[events.size() + 1];
+     return process(events);
+   }
+
+   private List<Student> process(List<String> events) {
+     List<Student> result = new ArrayList<>();
+     for (String e: events) {
+       if (e.startsWith("ENTER")) {
+         String[] data = e.split(" ");
+         Student st = new Student(Integer.parseInt(data[3]), data[1], Double.parseDouble(data[2]));
+         insert(st);
+       } else if (e.equals("SERVED")) {
+         result.add(extract());
+       } else {
+          //throw new Exception("Bad instruction: " + e);
+       }
+     }
+     return dumpReverse();
+   }
+
+   private List<Student> dumpReverse() {
+       List<Student> result = new ArrayList<>();
+       for (int i = n; i >= 1; i--) {
+           result.add(a[i]);
+       }
+       return result;
+
+   }
+
+   private void dump() {
+       System.out.println("Dump ****");
+       for (int i = 1; i <= n; i++) {
+           System.out.println(a[i]);
+           //a[i].toString();
+       }
+       System.out.println("End dump ***");
+   }
+
+   private void insert(Student s) {
+     //a[++n] = s;
+     int k = ++n;
+     a[k] = s;
+     while (k > 1 && s.compareTo(a[k / 2]) <= 0) {
+       a[k] = a[k/2];
+       k = k / 2;
+     }
+     a[k] = s;
+     if (n % 2 == 1 && n != 1) {
+         if (a[n].compareTo(a[n - 1]) < 0)
+         {
+             Student tmp = a[n];
+             a[n] = a[n - 1];
+             a[n - 1] = tmp;
+         }
+     }
+     //dump();
+   }
+
+   private Student extract() {
+      Student s = a[n--];
+      return s;
+   }
+}
 
 
 public class Solution {
